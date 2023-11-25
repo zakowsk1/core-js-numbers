7832026cead77fd5837a25549c8edab55c6eda6b
Vlad Zakorukin <149900039+zakowsk1@users.noreply.github.com>
2023-11-25 21:02:16 +0300

    feat: solve tasks 1-4

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
@@ -33,8 +33,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * (radius * Math.PI);
 }
 
 /**
@@ -49,8 +49,12 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  if (value1 !== Number.MAX_VALUE || value2 !== Number.MAX_VALUE) {
+    const sum = value1 + value2;
+    return sum / 2;
+  }
+  return Number.MAX_VALUE - Math.abs((value1 - value2) / 2);
 }
 
 /**
@@ -68,8 +72,9 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const AB = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return AB;
 }
 
 /**
